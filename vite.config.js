d7f94e78686eee2ea0941c84f67dfe2b8469f32d
kahkashanshaik <kahkashan1507@gmail.com>
2024-12-11 18:28:09 +0530

    add vite-plugin-rewrite

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path';
+import rewriteAll from 'vite-plugin-rewrite-all';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(),rewriteAll()],
   resolve: {
     alias: {
       '@': path.relative(__dirname, 'src'),
