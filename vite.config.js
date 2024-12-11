1a06ccf1a240187e627b041065a82ecff8c97f2d
kahkashanshaik <kahkashan1507@gmail.com>
2024-12-11 20:06:59 +0530

    bug fix

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path';
-import rewriteAll from 'vite-plugin-rewrite-all';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(),rewriteAll()],
+  plugins: [react()],
   resolve: {
     alias: {
       '@': path.relative(__dirname, 'src'),
